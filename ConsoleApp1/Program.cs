510dd325da3c15d22bc3b45b678db1798dcc3694
kseniia-dziuba-NURE <kseniia.dziuba@nure.ua>
2018-12-03 17:41:08 +0200

    "remove warnings"

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,14 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Matrix matrix = new Matrix();
             matrix.Restrictions = 4;
